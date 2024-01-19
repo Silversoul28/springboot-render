5398e25344c40bf4eecef1852feb8704ae1f1a45
Silversoul28 <brooklyn-scott@outlook.com>
2024-01-18 21:46:57 -0800

    docker change

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,3 +11,19 @@ public class DemoApplication {
 	}
 
 }
+
+//to put on github type git init into terminal
+//git remote add "name" and then the repository url
+//git add .
+//git commit -m "message""
+//git push "name" main
+
+//git config user.name "new name"
+//git config credential.username "new name"
+
+//git pull origin main --allow-unrelated-histories
+// git push -u origin main
+
+
+//start.spring.io
+//docker maven openjdk 17
